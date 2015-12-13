bde11e1182774f50b7dfc62773befcf62ace3553
jvrodrigo <josevicenterodrigodelgado@gmail.com>
2015-12-13 13:53:50 +0100

    Intentando añadir cookies "No funciona en OpenShift

---
+++ WebContent/js/utils.js
@@ -0,0 +1,16 @@
+// Inserta el nombre de usuario guardada en la cookie en el formulario POST para volver a la SalaPrincipal
+// var cookieUserName = getCookie("userName");
+// document.getElementById("formUserName").value = cookieUserName;
+// 	var exdays = 1;// 1 dia expira la cookie
+// 	var now = new Date(Date.now() + exdays*24*60*60*1000);
+// 	document.cookie = "userName=" + myName.innerHTML + "; expires=" + now.toUTCString();
+//function getCookie(cname) {
+//    var name = cname + "=";
+//    var ca = document.cookie.split(';');
+//    for(var i=0; i<ca.length; i++) {
+//        var c = ca[i];
+//        while (c.charAt(0)==' ') c = c.substring(1);
+//        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+//    }
+//    return "";
+//}
